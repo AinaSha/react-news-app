0698de2dacfed901ef199dd8cb95395d8bc6f984
Ainagul Shabdanova <shabdanova.ainagul.kk@gmail.com>
2023-04-05 08:13:51 +0600

    feat: create news-app

---
+++ src/components/Home/Error.tsx
@@ -0,0 +1,14 @@
+import { Typography } from "@mui/material";
+
+interface ErrorProps {
+  error: string;
+}
+const Error = ({ error }: ErrorProps) => {
+  return (
+    <Typography variant="subtitle1" sx={{ fontStyle: "italic", color: "red" }}>
+      {error}
+    </Typography>
+  );
+};
+
+export default Error;
